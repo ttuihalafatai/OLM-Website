228577d807dd6a15cfaad534b561fd34b6a34102
Caroline Paulic <carolinepaulic@eworldes.com>
2015-11-22 17:21:42 -1000

    Added basic page structure and hooked up nav bar

--- js/includes.js
+++ js/includes.js
@@ -1,7 +1,9 @@
 //Loads common elements when page is loaded.
 (function() {
     var loadIncludes = function() {
+        $("#head-resources").load("head.html");
         $("#navigation").load("navigation.html"); //The div to replace followed by the name of the file to replace it with
+        $("home").load("home.html");
     };
     $(document).ready(function() {
         loadIncludes();
